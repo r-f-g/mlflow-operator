1bd8515ef5e93934f90231037a19fd7e58960c51
Robert Gildein <gildeinrobert@gmail.com>
2021-06-21 08:13:42 +0200

    MLflow Tracking server & Model Registry (#1)
    * add unit tests
    
    * applied suggestion + add minio to create bucket
    
    * add README.md
    
    * update requirements and catch Pebble API error

--- README.md
+++ README.md
@@ -1,12 +1,116 @@
 # mlflow
 
+**MLflow** is an open source platform for managing the end-to-end machine learning lifecycle.
+
 ## Description
 
-TODO: Describe your charm in a few paragraphs of Markdown
+This charm provides **MLflow Tracking Server** and **Model Registry**, that can be described as
+these functions:
+ * Tracking experiments to record and compare parameters along with the model results.
+ * Providing a central model store to collaboratively manage the full lifecycle of an MLflow Model, 
+   including model versioning, stage transitions, and annotations.
+   
+For more information visit [MLflow Documentation][MLflow-docs]
 
 ## Usage
 
-TODO: Provide high-level usage, such as required config or relations
+Right now there is no published version in [charmhub] and it needs to be
+manually built with `charmcraft`.
+
+    charmcraft build -v
+    juju deploy ./mlflow.charm --resource server=docker.io/blueunicorn90/mlflow-operator:latest
+
+The `mlflow-operator` image is simple image based on the `python:3.9-slim` image together with
+the following python packages: 
+
+ * mlflow >= 1.0
+ * pymysql  # for connection with MySQL
+ * boto3  # for connection with Minio
+
+The use of deployed **mlflow** requires the following environmental variable:
+    
+    export MLFLOW_TRACKING_URI=http://<mlflow_ip>:<mlflow.config.port>
+
+### Usage with Minio [Optional]
+
+**Minio** is used as object storage for artifact store of **MLflow Tracking server**, where clients
+log their artifact output (e.g. models). 
+
+    juju deploy cs:minio-55
+    juju relate minio mlflow
+
+A bucket named `mlflow` is created and artifact data is stored in it.
+
+The use of deployed **mlflow** requires the following environmental variables:
+    
+    export MLFLOW_S3_ENDPOINT_URL=http://<minio_ip>:<minio.config.port>
+    export AWS_ACCESS_KEY_ID=<minio.config.access-key>
+    export AWS_SECRET_ACCESS_KEY=<minio.config.secret-key>
+    export MLFLOW_S3_IGNORE_TLS=true
+
+More information about artifact stores can be found [here][artifact-stores].
+
+---
+**NOTE:**
+By default, the file storage is used inside the sidecar container.
+---
+
+### Usage with MySQL [Optional]
+
+**MLflow Tracking Server** is database to stores experiment and run metadata as well as params,
+metrics, and tags for each run.
+
+    juju deploy cs:~charmed-osm/mariadb-k8s-35
+    juju relate mariadb-k8s mlflow
+
+---
+**NOTE:**
+By default, the **sqlite** database is used inside the sidecar container.
+---
+
+### Usage example
+
+Create and activate a virtualenv:
+
+    virtualenv -p python3 venv
+    source venv/bin/activate
+    pip install 'mlflow>=1.0'
+
+Define environmental variables:
+
+    export MLFLOW_TRACKING_URI=http://<mlflow_ip>:<mlflow.config.port>
+    export MLFLOW_S3_ENDPOINT_URL=http://<minio_ip>:<minio.config.port>
+    export AWS_ACCESS_KEY_ID=<minio.config.access-key>
+    export AWS_SECRET_ACCESS_KEY=<minio.config.secret-key>
+    export MLFLOW_S3_IGNORE_TLS=true
+
+Create python `train.py` script (`chmod +x train.py`):
+
+```python
+#!/usr/bin/env python
+import os
+import random
+import tempfile
+
+import mlflow
+
+
+with mlflow.start_run():
+    mlflow.log_param("param1", 1)
+    mlflow.log_param("param2", 2)
+    mlflow.log_metric("score", 0.8)
+    with tempfile.TemporaryDirectory() as tmpdir:
+        with open(os.path.join(tmpdir, "artifact-file"), "w") as file:
+            file.write(str(random.randint(0, 10)))
+
+        mlflow.log_artifacts(tmpdir)
+
+print("DONE")
+```
+
+Execute script:
+
+    ./train.py
 
 
 ## Developing
@@ -23,3 +127,26 @@ The Python operator framework includes a very nice harness for testing
 operator behaviour without full deployment. Just `run_tests`:
 
     ./run_tests
+
+## Roadmap
+
+* [x] MLflow Tracking Server
+  * [x] using local file system or Minio (via relation)
+  * [ ] make a configurable Minio bucket name
+* [x] MLflow Model Registry
+  * [x] using local database or MySQL (via relation)
+  * [ ] run database upgrade with `mlflow dp upgrade` command
+* [ ] MLflow Projects
+   * [ ] create/remove MLProject
+   * [ ] create templates for MLProject (using kubernetes as backend)
+   * [ ] create worker to train models (execute `mlflow run`)
+* [ ] MLflow Models
+   * [ ] create template for MLModels (using kubernetes as backend)
+   * [ ] create worker to deploy models (e.g. execute `mlflow server`)
+* [ ] functional tests
+
+
+---
+[MLflow-docs]: https://mlflow.org/docs/latest/index.html
+[charmhub]: https://charmhub.io/
+[artifact-stores]: https://mlflow.org/docs/latest/tracking.html#id69
--- src/charm.py
+++ src/charm.py
@@ -13,92 +13,257 @@ develop a new k8s charm using the Operator Framework:
 """
 
 import logging
+from typing import Optional
 
-from ops.charm import CharmBase
+import yaml
+from minio import Minio
+from ops.charm import (
+    CharmBase,
+    PebbleReadyEvent,
+    RelationBrokenEvent,
+    RelationChangedEvent,
+    ActionEvent,
+)
 from ops.framework import StoredState
 from ops.main import main
-from ops.model import ActiveStatus
+from ops.model import (
+    ActiveStatus,
+    BlockedStatus,
+    MaintenanceStatus,
+    RelationDataContent,
+    WaitingStatus,
+    ModelError,
+)
+from ops.pebble import TimeoutError, ConnectionError, APIError
+
+from serialized_data_interface import (
+    NoCompatibleVersions,
+    NoVersionsListed,
+    get_interfaces,
+)
+
+from charms.nginx_ingress_integrator.v0.ingress import IngressRequires
+
+DEFAULT_BACKEND_STORE_URI = "sqlite:///mlflow.db"
+DEFAULT_ARTIFACT_ROOT = "./mlruns"
+MINIO_BUCKET = "mlflow"
 
 logger = logging.getLogger(__name__)
 
 
 class MlflowCharm(CharmBase):
     """Charm the service."""
-
     _stored = StoredState()
 
     def __init__(self, *args):
         super().__init__(*args)
-        self.framework.observe(self.on.httpbin_pebble_ready, self._on_httpbin_pebble_ready)
+        # set interfaces
+        try:
+            self.interfaces = get_interfaces(self)
+        except NoVersionsListed as error:
+            # This is a transient error that will be resolved in the next run.
+            self.model.unit.status = WaitingStatus(str(error))
+            return
+        except NoCompatibleVersions as error:
+            self.model.unit.status = BlockedStatus(str(error))
+            return
+        except ModelError:
+            # if minio relation is removed
+            #   ops.model.ModelError: b'ERROR "" is not a valid unit or application\n'
+            pass
+        # install operator and prepare services
+        self.framework.observe(self.on.install, self._on_install)
+        self.framework.observe(self.on.server_pebble_ready, self._on_server_pebble_ready)
+        # configurations
         self.framework.observe(self.on.config_changed, self._on_config_changed)
-        self.framework.observe(self.on.fortune_action, self._on_fortune_action)
-        self._stored.set_default(things=[])
+        # actions
+        self.framework.observe(self.on.db_upgrade_action, self._dp_upgrade_action)
+        # relations
+        self.framework.observe(self.on.mysql_relation_changed, self._on_mysql_relation_changed)
+        self.framework.observe(self.on.mysql_relation_broken, self._on_mysql_relation_broken)
+        self.framework.observe(self.on.object_storage_relation_changed,
+                               self._object_storage_relation_changed)
+        self.framework.observe(self.on.object_storage_relation_broken,
+                               self._object_storage_relation_broken)
+        # initialise stored state
+        self._stored.set_default(
+            backend_store_uri=DEFAULT_BACKEND_STORE_URI,
+            artifact_root=DEFAULT_ARTIFACT_ROOT,
+            minio_environment={})
+        # initialise ingress
+        self.ingress = IngressRequires(self, {
+            "service-hostname": self.config["host"],
+            "service-name": self.app.name,
+            "service-port": self.config["port"]
+        })
 
-    def _on_httpbin_pebble_ready(self, event):
-        """Define and start a workload using the Pebble API.
+    @staticmethod
+    def _create_bucket(endpoint: str, access_key: str, secret_key, secure: bool):
+        """Create bucket for MLflow server."""
+        client = Minio(endpoint, access_key=access_key, secret_key=secret_key, secure=secure)
+        if not client.bucket_exists(MINIO_BUCKET):
+            client.make_bucket(MINIO_BUCKET)
 
-        TEMPLATE-TODO: change this example to suit your needs.
-        You'll need to specify the right entrypoint and environment
-        configuration for your specific workload. Tip: you can see the
-        standard entrypoint of an existing container using docker inspect
+    def _on_install(self, _):
+        """Install on charm."""
+        self.unit.status = WaitingStatus("The Pebble plan need to be created.")
 
-        Learn more about Pebble layers at https://github.com/canonical/pebble
-        """
-        # Get a reference the container attribute on the PebbleReadyEvent
-        container = event.workload
-        # Define an initial Pebble layer configuration
-        pebble_layer = {
-            "summary": "httpbin layer",
-            "description": "pebble config layer for httpbin",
+    def _on_mysql_relation_changed(self, event: RelationChangedEvent):
+        """Handle DB relation changed event."""
+        if not self.unit.is_leader():
+            return
+
+        mysql: Optional[RelationDataContent] = event.relation.data.get(event.unit)
+        if "user" not in mysql:
+            self.unit.status = WaitingStatus("MySQL data are missing.")
+            return
+
+        # TODO: need to install pymysql to container or check if it's installed
+        self._stored.backend_store_uri = \
+            "mysql+pymysql://{user}:{password}@{host}:{port}/{database}".format(
+                user=mysql.get("user"), password=mysql.get("password"),
+                host=mysql.get("host"), port=mysql.get("port"), database=mysql.get("database")
+            )
+        self._on_config_changed(event)
+
+    def _on_mysql_relation_broken(self, event: RelationChangedEvent):
+        """Handle DB relation changed event."""
+        if not self.unit.is_leader():
+            return
+
+        self._stored.backend_store_uri = DEFAULT_BACKEND_STORE_URI
+        self._on_config_changed(event)
+
+    def _object_storage_relation_changed(self, event: RelationChangedEvent):
+        """Handle minio relation changed event."""
+        if not self.unit.is_leader():
+            return
+
+        secrets = yaml.safe_load(event.relation.data.get(event.app, {}).get("data", "{}"))
+        if "service" not in secrets:
+            self.unit.status = WaitingStatus("Minio data are missing.")
+            return
+
+        endpoint = f"{secrets['service']}:{secrets['port']}"
+        self._stored.minio_environment.update({
+            "MLFLOW_S3_ENDPOINT_URL": f"http://{endpoint}",
+            "AWS_ACCESS_KEY_ID": secrets["access-key"],
+            "AWS_SECRET_ACCESS_KEY": secrets["secret-key"],
+            "MLFLOW_S3_IGNORE_TLS": "false" if secrets["secure"] is True else "true"
+        })
+        self._stored.artifact_root = f"s3://{MINIO_BUCKET}/"
+        self._create_bucket(endpoint, secrets["access-key"],
+                            secrets["secret-key"], secrets["secure"])
+        self._on_config_changed(event)
+
+    def _object_storage_relation_broken(self, event: RelationBrokenEvent):
+        """Handle minio relation broken event."""
+        if not self.unit.is_leader():
+            return
+
+        self._stored.minio_environment = {}
+        self._stored.artifact_root = DEFAULT_ARTIFACT_ROOT
+        self._on_config_changed(event)
+
+    def _mlflow_layer(self):
+        """Returns a Pebble configuration layer for Mlflow."""
+        host = self.config["host"]
+        port = self.config["port"]
+        backend_store_uri = self._stored.backend_store_uri
+        artifact_root = self._stored.artifact_root
+        environment = dict(self._stored.minio_environment)
+
+        return {
+            "summary": "MLflow server layer",
+            "description": "pebble config layer for MLflow server",
             "services": {
-                "httpbin": {
+                "server": {
                     "override": "replace",
-                    "summary": "httpbin",
-                    "command": "gunicorn -b 0.0.0.0:80 httpbin:app -k gevent",
+                    "summary": "MLflow server",
+                    "command": "/bin/sh -c \"mlflow server "
+                               f"--host {host} "
+                               f"--port {port} "
+                               f"--backend-store-uri {backend_store_uri} "
+                               f"--default-artifact-root {artifact_root} "
+                                "|| exit 2\"",
                     "startup": "enabled",
-                    "environment": {"thing": self.model.config["thing"]},
+                    "environment": environment
                 }
-            },
+            }
         }
-        # Add intial Pebble config layer using the Pebble API
-        container.add_layer("httpbin", pebble_layer, combine=True)
-        # Autostart any services that were defined with startup: enabled
-        container.autostart()
-        # Learn more about statuses in the SDK docs:
-        # https://juju.is/docs/sdk/constructs#heading--statuses
+
+    def _manage_server_layer(self):
+        """Manage MLflow server layer with Pebble."""
+        container = self.unit.get_container("server")
+        mlflow_layer = self._mlflow_layer()
+        services = container.get_plan().to_dict().get("services", {})
+        actual_services = {service: {key: value for key, value in fields.items() if value}
+                           for service, fields in mlflow_layer["services"].items()}
+
+        if services != actual_services:
+            self.unit.status = MaintenanceStatus("MLflow server maintenance")
+            container.add_layer("mlflow-server", mlflow_layer, combine=True)
+            if container.get_service("server").is_running():
+                logging.info("Restarting MLflow server service")
+                container.stop("server")
+
+            container.start("server")
+
+    def _on_server_pebble_ready(self, event: PebbleReadyEvent):
+        """Start a workload using the Pebble API."""
+        # TODO: install mlflow in container or check if it's installed
+        try:
+            self._manage_server_layer()
+        except (TimeoutError, ConnectionError, APIError):
+            self.unit.status = BlockedStatus("Pebble API connection problem.")
+            event.defer()
+            return
+
+        if not event.workload.get_service("server").is_running():
+            self.unit.status = BlockedStatus("Mlflow server is not running.")
+            event.defer()
+            return
+
         self.unit.status = ActiveStatus()
 
     def _on_config_changed(self, _):
-        """Just an example to show how to deal with changed configuration.
+        """Handle the config-changed event."""
+        try:
+            self._manage_server_layer()
+        except (TimeoutError, ConnectionError, APIError):
+            self.unit.status = BlockedStatus("Pebble API connection problem.")
+            return
 
-        TEMPLATE-TODO: change this example to suit your needs.
-        If you don't need to handle config, you can remove this method,
-        the hook created in __init__.py for it, the corresponding test,
-        and the config.py file.
+        self.ingress.update_config({
+            "service-hostname": self.config["host"], "service-port": self.config["port"]
+        })
+        self.unit.status = ActiveStatus()
 
-        Learn more about config at https://juju.is/docs/sdk/config
-        """
-        current = self.config["thing"]
-        if current not in self._stored.things:
-            logger.debug("found a new thing: %r", current)
-            self._stored.things.append(current)
+    def _dp_upgrade_action(self, event: ActionEvent):
+        """Run MLflow dp upgrade."""
+        if not event.params.get("i-really-mean-it"):
+            event.fail("The 'i-really-mean-it' parameter must be toggled to enable actually "
+                       "performing this action.")
+            return
 
-    def _on_fortune_action(self, event):
-        """Just an example to show how to receive actions.
+        container = self.unit.get_container("server")
 
-        TEMPLATE-TODO: change this example to suit your needs.
-        If you don't need to handle actions, you can remove this method,
-        the hook created in __init__.py for it, the corresponding test,
-        and the actions.py file.
+        if not container.get_service("server").is_running():
+            event.fail("MLflow server is not running.")
+            return
 
-        Learn more about actions at https://juju.is/docs/sdk/actions
-        """
-        fail = event.params["fail"]
-        if fail:
-            event.fail(fail)
+        self.unit.status = MaintenanceStatus("Running MLflow db upgrade")
+        logger.info("Running MLflow db upgrade")
+        container.stop("server")
+        # TODO: run `mlflow db upgrade`
+        container.start("server")
+        if container.get_service("server").is_running():
+            event.set_results({"result": "MLflow database was upgrade"})
+            self.unit.status = ActiveStatus()
         else:
-            event.set_results({"fortune": "A bug in the code is worth two in the documentation."})
+            event.fail("MLflow server does not start after a restart.")
+            self.unit.status = BlockedStatus("MLflow server is not running")
 
 
 if __name__ == "__main__":
-    main(MlflowCharm)
+    main(MlflowCharm, use_juju_for_storage=True)
